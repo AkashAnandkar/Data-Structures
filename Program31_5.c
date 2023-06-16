f9aef337cc6f9f8354917ea310264036f6b8aa08
Akash Anandkar <129279254+AkashAnandkar@users.noreply.github.com>
2023-06-16 10:41:50 +0530

    Update Program31_5.c

--- Program31_5.c
+++ Program31_5.c
@@ -20,28 +20,50 @@ typedef struct Node NODE;
 typedef  struct Node * PNODE;
 typedef struct Node ** PPNODE;
 
+int Count(PNODE Head)
+{
+    int iCnt=0;
+    while(Head!=NULL)
+    {
+        iCnt++;
+        Head=Head->next;
+    }
+    return iCnt;
+}
+
 
 void SumDigit(PNODE Head)
 {
-    
+    int iLength=0;
+    int iCnt=0;
     int iSum=0;
     int iDigit=0;
-    int iNo;
+    int iNo1=0;
+    int iNo2=0;
+    PNODE temp=Head;
+    iLength=Count(Head);
 
     printf("Sum of Digits of each element is : \n");
 
-    while(Head!=NULL)
+    for(iCnt=1;iCnt<=iLength;iCnt++)
     {
-        while((Head->data)!=0)
-        {   
-            iDigit=((Head->data)%10);
+        iNo1=temp->data;
+        iNo2=iNo1;
+        while(iNo2!=0)
+        {
+            iDigit=iNo2%10;
             iSum=iSum+iDigit;
-            Head->data=((Head->data)/10);
+            iNo2=iNo2/10;
         }
         printf("%d\t",iSum);
-        Head=Head->next;
+
+
+        temp=temp->next;
         iSum=0;
     }
+    
+
+   
 }
 
 void InsertFirst(PPNODE Head,int No)
@@ -95,7 +117,14 @@ int main()
     
 
     Display(First);
-   SumDigit(First);
+
+    iRet=Count(First);
+    printf("Number of elements of linkedlist are : %d",iRet);
+
+    SumDigit(First);
+    
+    Display(First);
+
     
 
     return 0;
